3892d74152ea0b67d1fc6a98474ca1ebfbe3e4aa
Kavit Patel <kvpatel.er@gmail.com>
2024-05-27 14:11:01 +0530

    Home page done

--- apps/restaurant/src/component/Navbar.tsx
+++ apps/restaurant/src/component/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { BsCartCheckFill } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [active, setActive] = useState<string>("");
@@ -32,25 +33,29 @@ const Navbar = () => {
         <div
           className={`md:flex list-none text-xs ${view ? "flex flex-col justify-center items-center gap-2 absolute top-9 left-0 w-full h-[calc(100vh-40px)] bg-[#E5E5E5]" : "hidden  w-[70%]  h-full justify-end items-center "} `}
         >
-          <li
+          <Link
+            to="/"
             onClick={() => setActive("Home")}
             className={`w-28 h-9 transition-all hover:font-semibold flex justify-center items-center cursor-pointer ${active === "Home" ? " text-sm rounded-md bg-[#FC8A06] font-semibold text-white" : ""}`}
           >
             Home
-          </li>
-          <li
+          </Link>
+          <Link
+            to="#"
             onClick={() => setActive("Browse")}
             className={`w-28 h-9 transition-all hover:font-semibold  flex justify-center items-center cursor-pointer ${active === "Browse" ? " text-sm rounded-md bg-[#FC8A06] font-semibold text-white" : ""}`}
           >
             Browse Menu
-          </li>
-          <li
+          </Link>
+          <Link
+            to="#"
             onClick={() => setActive("About")}
             className={`w-28 h-9 transition-all hover:font-semibold  flex justify-center items-center cursor-pointer ${active === "About" ? " text-sm rounded-md bg-[#FC8A06] font-semibold text-white" : ""}`}
           >
             About
-          </li>
-          <li
+          </Link>
+          <Link
+            to="#"
             onClick={() => setActive("Cart")}
             className={`w-28 h-9 transition-all hover:font-semibold  flex justify-center items-center cursor-pointer ${active === "Cart" ? " text-sm rounded-md bg-[#FC8A06] font-semibold text-white" : ""}`}
           >
@@ -58,13 +63,14 @@ const Navbar = () => {
               <BsCartCheckFill />
             </span>
             <span>Cart</span>
-          </li>
-          <li
+          </Link>
+          <Link
+            to="#"
             onClick={() => setActive("login")}
             className={`w-28 h-9 transition-all hover:font-semibold  flex justify-center items-center cursor-pointer ${active === "login" ? " text-sm rounded-md bg-[#FC8A06] font-semibold text-white" : ""}`}
           >
             Login/Sign-up
-          </li>
+          </Link>
         </div>
       </div>
     </div>
--- apps/restaurant/src/pages/Home.tsx
+++ apps/restaurant/src/pages/Home.tsx
@@ -1,5 +1,28 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <div
+        style={{ backgroundImage: "url('/images/restaurant.png')" }}
+        className="w-full h-[calc(100vh-36px)] flex flex-col gap-4 text-white justify-center items-center"
+      >
+        <div className=" text-3xl md:text-4xl lg:text-5xl">
+          Welcome to <span className="font-bold">Shushi</span> Restaurant
+        </div>
+        <div className="w-[65%] md:w-[58%] lg:w-[50%] text-center text-sm">
+          People eat with their eyes and Sushi creates an easy way for customers
+          to order when they can see beautiful photos of your food
+        </div>
+        <div className="text-black flex gap-3">
+          <button className="bg-white w-24 py-2 rounded-sm font-mono transition-all hover:bg-slate-600 hover:text-white hover:font-semibold active:scale-95">
+            About
+          </button>
+          <button className="bg-white w-24 py-2 rounded-sm font-mono transition-all hover:bg-slate-600 hover:text-white hover:font-semibold active:scale-95">
+            Menu
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
